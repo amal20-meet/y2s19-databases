d318ed72f552589270f237e07ff1a231f29c0c10
myname21-meet <myname21@meet.mit.edu>
2019-08-01 07:17:29 -0400

    j

--- knowledge_databases.py
+++ knowledge_databases.py
@@ -8,20 +8,43 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(topic,name,rating):
+	Narticle = Knowledge(
+		topic = topic,
+		name = name,
+		rating = rating)
+	session.add(Narticle)
+	session.commit()
+	
+# add_article("stuff", "Zain", 10)
+# add_article("hello", "Juna", 2)
+# add_article("Share", "Amal", 10)
+# add_article("Unicorn", "Shir", 9)
+# add_article("Whatever", "Someone", 5)
 
 def query_all_articles():
-	pass
 
-def query_article_by_topic():
-	pass
+	all_article = session.query(Knowledge).all()
+	return all_article
 
-def delete_article_by_topic():
-	pass
+# print(query_all_articles())
+
+def query_article_by_topic(their_topic):
+	article_by_topic = session.query(Knowledge).filter_by(topic = their_topic).first()
+	return article_by_topic
+
+# print(query_article_by_topic("stuff"))
+
+def delete_article_by_topic(their_topic):
+	session.query(Knowledge).filter_by(topic=their_topic).delete()
+	session.commit()
 
 def delete_all_articles():
-	pass
+	session.query(Knowledge).delete()
+	session.commit()
 
-def edit_article_rating():
-	pass
+def edit_article_rating(title,x):
+	knowledge = session.query(Knowledge).filter_by(topic = title).first()
+	knowledge.rating=x
+	session.commit()
+
--- knowledge_model.py
+++ knowledge_model.py
@@ -6,6 +6,23 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
+
+	__tablename__ = 'Wikipedia'
+	knowledge_id = Column(Integer,primary_key=True)
+	name = Column(String)
+	topic = Column(String)
+	rating = Column(Integer)
+
+
+	def __repr__(self):
+		return("If you want to learn about: {}\n"
+               "you should look at the Wikipedia article called: {} \n"
+               "We gave this article a rating of : {}\n ""out of 10" ).format(
+                    self.topic, self.name, self.rating)
+
+
+	
+
 	# Create a table with 4 columns
 	# The first column will be the primary key
 	# The second column should be a string representing
@@ -13,5 +30,3 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
-
-	pass
